84334ca6cec2ae06dcf03f8993f670b63915ec57
Vikash kumar <vtech076@gmail.com>
2025-09-21 22:31:12 +0530

    cores fix

--- backend/index.js
+++ backend/index.js
@@ -18,11 +18,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
  
-const corsOptions = {
-    origin: '*', 
-    credentials: true, 
-};
-app.use(cors(corsOptions));
+app.use(cors({
+    origin: ['http://localhost:5173', 'https://job-hunt-main-bjn2.vercel.app'],
+    credentials: true
+}));
+// app.use(cors(corsOptions));
 
 const PORT = process.env.PORT || 3000;
 app.use('/api/v1/user', userRoutes);
